2b17f76965b1c4a2fd111dec89d447a2f5b575ce
3noch <elliot@3noch.com>
2012-05-19 21:09:08 -0400

    Refactor Haskell a bit.

--- haskell/lib/bayes-on-redis.hs
+++ haskell/lib/bayes-on-redis.hs
@@ -1,111 +1,97 @@
 module Main where
 
 import Control.Monad
-import qualified Data.ByteString.Char8 as Bs
-import qualified Database.Redis as Rs
+import Data.ByteString.Char8 (ByteString,
+                              append,
+                              pack,
+                              readInteger,
+                              splitWith)
+import Database.Redis as Rs hiding (append, sort)
+import Data.Char
 import Data.List
 
-type Category = Bs.ByteString
-type Document = Bs.ByteString
-type Word     = Bs.ByteString
-type Tag      = Bs.ByteString
+type Category = ByteString
+type Document = ByteString
+type Word     = ByteString
+type Tag      = ByteString
 
 
-redisConfig :: Rs.ConnectInfo
-redisConfig = Rs.defaultConnectInfo
-    { Rs.connectHost = "localhost"
-    , Rs.connectPort = Rs.PortNumber 6379
+redisConfig :: ConnectInfo
+redisConfig = defaultConnectInfo
+    { connectHost = "localhost"
+    , connectPort = PortNumber 6379
     }
 
 
 train :: Category -> Document -> IO ()
 train cat doc = do
-    conn <- Rs.connect redisConfig
-    Rs.runRedis conn $ do
+    conn <- connect redisConfig
+    runRedis conn $ do
         addCategory cat
         insertDocument cat doc
 
 
 untrain :: Category -> Document -> IO ()
 untrain cat doc = do
-    conn <- Rs.connect redisConfig
-    Rs.runRedis conn $ removeDocument cat doc
+    conn <- connect redisConfig
+    runRedis conn $ removeDocument cat doc
 
 
-addCategory :: Category -> Rs.Redis ()
+addCategory :: Category -> Redis ()
 addCategory cat = do
-    Rs.sadd getCategoriesTag [cat]
+    sadd getCategoriesTag [cat]
     return ()
 
 
-applyDocumentWith :: (Tag -> (Word, Integer) -> Rs.Redis ())
+applyDocumentWith :: (Tag -> (Word, Integer) -> Redis ())
                      -> Category
                      -> Document
-                     -> Rs.Redis ()
+                     -> Redis ()
 applyDocumentWith modifier cat doc = mapM_ (modifier tag) (countOccurrence doc)
     where tag = getRedisCategoryTag cat
 
 
-insertDocument :: Category -> Document -> Rs.Redis ()
+insertDocument :: Category -> Document -> Redis ()
 insertDocument = applyDocumentWith insertWord
     where
-        insertWord :: Tag -> (Word, Integer) -> Rs.Redis ()
         insertWord tag (word, count) = do
-            Rs.hincrby tag word count
+            hincrby tag word count
             return ()
 
 
-removeDocument :: Category -> Document -> Rs.Redis ()
+removeDocument :: Category -> Document -> Redis ()
 removeDocument = applyDocumentWith removeWord
     where
-        removeWord :: Tag -> (Word, Integer) -> Rs.Redis ()
         removeWord tag (word, count) = do
-            response <- Rs.hget tag word
+            response <- hget tag word
             case readRedisInteger response of
-                Just old -> do
-                    Rs.hset tag word (integerToBs $ max 0 (old - count))
+                Just old -> do -- TODO delete zero-keys
+                    hset tag word (integerToBs $ max 0 (old - count))
                     return ()
-                Nothing       -> return ()
+                Nothing  -> return ()
 
 
 countOccurrence :: Document -> [(Word, Integer)]
-countOccurrence = countWordOccurrence . splitDocument
+countOccurrence = map makeTuple . group . sort . splitDocument
+    where makeTuple xs@(first:_) = (first, genericLength xs)
+          splitDocument = splitWith (not . isAlphaNum)
 
 
-countWordOccurrence :: [Word] -> [(Word, Integer)]
-countWordOccurrence = map makeTuple . group . sort
-    where
-        makeTuple :: [Word] -> (Word, Integer)
-        makeTuple xs@(first:_) = (first, genericLength xs)
-
-
-splitDocument :: Document -> [Word]
-splitDocument = Bs.splitWith (not . isAlphaNum)
-
-
-alphanums :: String
-alphanums = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
-
-
-isAlphaNum :: Char -> Bool
-isAlphaNum = (`elem` alphanums)
-
-
-readRedisInteger :: Either Rs.Reply (Maybe Bs.ByteString) -> Maybe Integer
+readRedisInteger :: Either Reply (Maybe ByteString) -> Maybe Integer
 readRedisInteger (Right (Just str)) =
-    case Bs.readInteger str of
+    case readInteger str of
         (Just (val, _)) -> Just val
-        Nothing         -> Nothing
+        _               -> Nothing
 readRedisInteger _ = Nothing
 
 
-integerToBs :: Integer -> Bs.ByteString
-integerToBs = Bs.pack . show
+integerToBs :: Integer -> ByteString
+integerToBs = pack . show
 
 
 getCategoriesTag :: Tag
-getCategoriesTag = Bs.pack "BayesOnRedis:categories"
+getCategoriesTag = pack "BayesOnRedis:categories"
 
 
 getRedisCategoryTag :: Category -> Tag
-getRedisCategoryTag = Bs.append (Bs.pack "BayesOnRedis:cat:")
+getRedisCategoryTag = append (pack "BayesOnRedis:cat:")
